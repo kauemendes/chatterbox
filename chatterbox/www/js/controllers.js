4044b44346eccfed0af629c693a54e8b1d05edd1
Maikom James <maikom.silva@pearson.com>
2016-07-29 15:55:29 -0300

    adicionando tela de assessments

--- chatterbox/www/js/controllers.js
+++ chatterbox/www/js/controllers.js
@@ -4,12 +4,22 @@ angular.module('starter.controllers', ['starter.services'])
     console.log("Loaded");
 })
 
+.controller('Assessment', function($scope, LoginService, $ionicPopup, $state) {
+    console.log("Assessment Loaded");
+    $scope.data = {};
+
+    $scope.next = function() {
+      $state.go('assessment-interest');
+    };
+
+})
+
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {};
 
     $scope.login = function() {
         LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-            $state.go('app.sessions');
+            $state.go('assessment');
         }).error(function(data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
