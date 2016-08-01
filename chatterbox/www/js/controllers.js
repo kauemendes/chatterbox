8d7752246ce96f886dac7e00ca10fb20410dee5c
Maikom James <maikom.silva@pearson.com>
2016-08-01 11:46:27 -0300

    tela de login

--- chatterbox/www/js/controllers.js
+++ chatterbox/www/js/controllers.js
@@ -89,14 +89,7 @@ angular.module('starter.controllers', ['starter.services', 'ionic'])
     $scope.data = {};
 
     $scope.login = function() {
-        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-            $state.go('app.start');
-        }).error(function(data) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Login failed!',
-                template: 'Please check your credentials!'
-            });
-        });
+        $state.go('app.start');
     }
 })
 
